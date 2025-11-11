a2d780294ebbbd55e1a631150ce7c0a3f00c42e8
Gabex_israel <153013936+Gabx-israel@users.noreply.github.com>
2025-11-11 11:20:55 -0600

    just testing different layout

--- frontend/src/pages/about/about.jsx
+++ frontend/src/pages/about/about.jsx
@@ -1,4 +1,11 @@
-// group11project/frontend/src/pages/about/about.jsx
+// group11project/frontend/src/pages/about/about.jsx`
+//* in the about page we must detail what our volunteer services offer
+//* 1. Task urgency and priority
+//* 2. user proper notification and messaging paths
+//* 3. user match making capabilities
+//* 4. volunteer history
+//* 
+//*
 import React from "react";
 import "./about.css";
 import Layout from "../../components/layout.jsx"; // note the .jsx
@@ -6,10 +13,13 @@ import Layout from "../../components/layout.jsx"; // note the .jsx
 export default function About({ isLoggedIn, onLogout, user }) {
   return (
     <Layout currentPage="about" isLoggedIn={isLoggedIn} onLogout={onLogout} user={user}>
-      <section className="container" style={{ padding: "2rem 1rem" }}>
-        <h1>About Houston Hearts</h1>
+      <section className="flex">
+        <h1 className="items-center ">About Houston Hearts</h1>
         <p>We connect volunteers with local events using skills, availability, and proximity.</p>
       </section>
     </Layout>
   );
 }
+
+
+//! properly connected now!!!!
