1ff4e8f99ec6c73d123231b0ba0530471d4381eb
Gabex_israel <153013936+Gabx-israel@users.noreply.github.com>
2025-11-12 17:29:19 -0600

    adjusting layout

--- frontend/src/pages/about/about.jsx
+++ frontend/src/pages/about/about.jsx
@@ -15,7 +15,6 @@ export default function About({ isLoggedIn, onLogout, user }) {
   
   return (
     <Layout currentPage="about" isLoggedIn={isLoggedIn} onLogout={onLogout} user={user}>
-      <section>
         {/* okay so this fixed the margin issue i was having just now giving each portion its own section */}
         <section className="bg-blue-500" > 
           <h1 className="flex items-center justify-center font-bold mb-3">
@@ -59,9 +58,6 @@ export default function About({ isLoggedIn, onLogout, user }) {
             </p>
           </div>
         </div> */}
-
-
-      </section>
     </Layout>
   );
 }
