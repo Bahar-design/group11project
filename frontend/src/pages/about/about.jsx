9fb3af2b98ecedce43bc1c560cb9bdbfe1e59115
Gabex_israel <153013936+Gabx-israel@users.noreply.github.com>
2025-11-12 15:05:48 -0600

    adjusting layout

--- frontend/src/pages/about/about.jsx
+++ frontend/src/pages/about/about.jsx
@@ -20,9 +20,9 @@ export default function About({ isLoggedIn, onLogout, user }) {
         <h1 className="flex items-center justify-center font-bold mb-4">About Houston 
           <span className="text-red-500 font-bold ml-2"> Hearts</span>
         </h1>
-        
-        <div className="flex items-center justify-center mb-[200px]">
-          <p>
+
+        <div className="mb-20">
+          <p className="text-center max-w-2xl mx-auto">
             We connect volunteers with local events using skills, availability, and proximity –– making it easier to give back to the community
           </p>
         </div>
