1ff3eb46239d4b7b462c4d132e109228fef11b19
Gabex_israel <153013936+Gabx-israel@users.noreply.github.com>
2025-11-12 14:38:10 -0600

    adjusting layout

--- frontend/src/pages/about/about.jsx
+++ frontend/src/pages/about/about.jsx
@@ -40,7 +40,7 @@ export default function About({ isLoggedIn, onLogout, user }) {
             <img
               src={volunteerImg}
               alt="volunteer-image"
-              className="w-[400px] h-[350px]" 
+              className="w-5400px] h-[350px]" 
             />
           </div>
         </div>
