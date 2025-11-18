49705bf6cb5c799881a1403f04173a55b1abb627
Gabex_israel <153013936+Gabx-israel@users.noreply.github.com>
2025-11-18 12:42:50 -0600

    hooked up the server.js to only http and localhost allowed to make req's

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,15 @@ const cors = require("cors");
 const pool = require("./db");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin:[
+      process.env.FRONTEND_ORIGIN,
+      "http://localhost:5173"
+    ],
+    credential : true,
+  })
+);
 app.use(express.json());
 
 // ✅ Test route to confirm DB connection
